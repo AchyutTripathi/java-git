ecf2710cb96f1ebc69218e2ca56a9d541dfb769f
Achyut Tripathi <achyuttripathi06@gmail.com>
2025-07-21 16:46:01 +0530

    Add files via upload

---
+++ Google & Amazon/Leetcode17.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+public class Leetcode17 {
+    public static void main(String[] args) {
+        pad("", "12");
+        System.out.println(padRet("", "12").size());
+        System.out.println(padCount("", "12"));
+    }
+
+    static void pad(String p, String up) {
+        if(up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i); // this will convert 0 into 'a'
+            pad(p + ch, up.substring(1)); // this will call the function with the next digit
+        }
+    }
+
+
+
+
+
+
+
+
+    // return type is ArrayList
+    static ArrayList<String> padRet(String p, String up) {
+        if(up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(padRet(p + ch, up.substring(1)));
+        }
+        return list;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // return the count
+    static int padCount(String p, String up) {
+        if(up.isEmpty()) {
+            System.out.println(p);
+            return 1;
+        }
+
+        int count = 0;
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            count = count + padCount(p + ch, up.substring(1));
+        }
+        return count;
+    }
+
+}
